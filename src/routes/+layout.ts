8e17a0b7eb6b7d01ee179566ae45832ffaee05bc
Fraser Croad <fraser.croad@gmail.com>
2025-07-11 23:03:37 +0200

    Add github pages options

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,7 @@ import type { LayoutLoad } from './$types';
 import { getSessions } from '$lib/queries';
 
 export const ssr = false;
+export const prerender = true;
 
 export const load: LayoutLoad = async ({ params, fetch }) => {
 	try {
